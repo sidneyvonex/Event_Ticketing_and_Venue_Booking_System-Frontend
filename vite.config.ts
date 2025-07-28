6921849fa1b64d4adb70cc8e0fda91f78c6ffba9
bensidney ndungu <sidneyvonex@gmail.com>
2025-07-28 16:24:35 +0300

    feat: update package.json and pnpm-lock.yaml to include vite-plugin-compression and set node engine requirement

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import compression from 'vite-plugin-compression';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    compression(), // Adds gzip compression
   ],
-})
+  build: {
+    minify: 'esbuild',
+    sourcemap: false, // Prevents large .map files
+  },
+});
